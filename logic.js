49967b558ac6f9b8829fee3de71114770069d6c8
Andrew <barber87@hotmail.co.uk>
2022-09-23 22:38:53 +0100

    added logic to post random selections for 2 players

--- logic.js
+++ logic.js
@@ -1,8 +1,25 @@
 console.log('test');
 
 //set the options available to log
-const getComputerChoice = ["rock", "paper", "scissors"];
+const getComputerChoice = ["c-rock", "c-paper", "c-scissors"];
 //choose one at random
-const random = Math.floor(Math.random() * getComputerChoice.length);
+const randomComputer = Math.floor(Math.random() * getComputerChoice.length);
 //log the selection in console
-console.log(random, getComputerChoice[random]);
+console.log(randomComputer, getComputerChoice[randomComputer]);
+
+
+const getPlayerChoice = ["p-rock", "p-paper", "p-scissors"];
+//choose one at random
+const randomPlayer = Math.floor(Math.random() * getPlayerChoice.length);
+//log the selection in console
+console.log(randomPlayer, getPlayerChoice[randomPlayer]);
+
+//generate the player selection
+//log the player selection to console
+
+//generate the compuer selection
+//log the computer selection to console
+
+//compare the selections
+//declare a winner
+//post winner to the console
