95139401acfffbe33c56cc087a75f3ac1e1e1f3c
Andrew <barber87@hotmail.co.uk>
2022-09-23 22:07:15 +0100

    tidied up code and comments

--- logic.js
+++ logic.js
@@ -1,11 +1,8 @@
 console.log('test');
 
 //set the options available to log
+const getComputerChoice = ["rock", "paper", "scissors"];
 //choose one at random
+const random = Math.floor(Math.random() * getComputerChoice.length);
 //log the selection in console
-
-
-
-const choice = ["rock", "paper", "scissors"];
-const random = Math.floor(Math.random() * choice.length);
-console.log(random, choice[random]);
+console.log(random, getComputerChoice[random]);
