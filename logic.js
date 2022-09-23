22153d9c9b665bb20e1fc93bae5c5e97ae012797
Andrew <barber87@hotmail.co.uk>
2022-09-23 21:57:36 +0100

    initial commit with random choice logi

---
+++ logic.js
@@ -0,0 +1,11 @@
+console.log('test');
+
+//set the options available to log
+//choose one at random
+//log the selection in console
+
+
+
+const choice = ["rock", "paper", "scissors"];
+const random = Math.floor(Math.random() * choice.length);
+console.log(random, choice[random]);
